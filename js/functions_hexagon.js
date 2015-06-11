72d9fcac0f5f5272847287dbf440f57fb33e10ad
rogegg <rogegg@correo.ugr.es>
2015-06-11 16:25:15 +0200

    Merge para actualizar

--- js/functions_hexagon.js
+++ js/functions_hexagon.js
@@ -6,6 +6,7 @@ function leerHexagono(json){
     context_hexagono.pregunta = new Array();
     var v_pregunta = new Array();
     var v_opciones = new Array();
+    var v_opciones2 = new Array();
     var opciones = new Array();    
     var respuesta = new Array();
     var total = json.feed.entry.length;
@@ -34,17 +35,23 @@ function leerHexagono(json){
         //Nueva pregunta
         if(String(json.feed.entry[i].gsx$pregunta.$t).length > 0){
             nombre_pregunta_tmp = json.feed.entry[i].gsx$pregunta.$t;
-            //recorremos las 7 opciones (de la 0 a la 6)            
-            for(var j=i ; j==i || json.feed.entry[j].gsx$pregunta.$t == "" ; j++){ //revisar, error en la última posición de j.
+//creando la función generaOpciones para esta sección
+            //recorremos las opciones
+            v_opciones2 = generaOpciones(json,i);
+            for(var j=i ; (j==i || json.feed.entry[j].gsx$pregunta.$t == "") && j<(total-1) ; j++){ //revisar, error en la última posición de j.
                 //console.log("J++   ->  "+(j+1);
                 v_opciones.push({
-                   nombre_opcion: json.feed.entry[j].gsx$opciones.$t
+                    nombre_opcion: json.feed.entry[j].gsx$opciones.$t,
+                    id_opcion: j,
+                    codigo: json.feed.entry[j].gsx$codificación.$t
                 });            
-                //console.log(j+"->"+String(json.feed.entry[j].gsx$pregunta.$t).length);
+                console.log(j+"->"+json.feed.entry[j].gsx$pregunta.$t);
+                console.log(total);
             }
             //console.log(v_opciones);
             v_pregunta.push({
                 nombre_pregunta: nombre_pregunta_tmp,
+                id_pregunta: i,
                 opcion: v_opciones
             });
             v_opciones=[];
@@ -53,4 +60,40 @@ function leerHexagono(json){
     context_hexagono.pregunta = v_pregunta;
     
     console.log(context_hexagono);
+}
+
+
+//Genera las opciones de cada pregunta.
+//La estructura de las opciones es la siguiente
+//
+//   Opcion:
+//      - nombre_opción (string)
+//      - id_opción     (string)
+//      - código        (array)
+//
+// El nombre_opción no puede estar repetido dentro de una misma opción.
+// Una misma opción puede tener varios códigos.
+// Recibe:
+//   - json: estructura json de toda la página spreadseet de drive.
+//   - indice: indice por el que empezar a recorrer el json.
+function generaOpciones(json,indice){
+    var i = indice;
+    var total = json.feed.entry.length; 
+    var nombre = new Array();
+    var id = new Array();
+    var codigo = new Array();
+    
+    for(var j=i ; (j==i || json.feed.entry[j].gsx$pregunta.$t == "") && j<(total-1) ; j++){ //revisar, error en la última posición de j.
+        nombre.push(json.feed.entry[j].gsx$opciones.$t);
+        id.push(j);
+        codigo.push(json.feed.entry[j].gsx$codificación.$t);
+        
+        v_opciones.push({
+            nombre_opcion: json.feed.entry[j].gsx$opciones.$t,
+            id_opcion: j,
+            codigo: json.feed.entry[j].gsx$codificación.$t
+        });            
+    }
+    
+    
 }
